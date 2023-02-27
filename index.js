b4e26e236896745a9b48e1dfd9022ba4acf27eb3
za-heer <mydocument783@gmail.com>
2023-02-27 11:02:11 +0500

    Native backend

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const dbConnection = require('./Config/dbConnection');
 const app = express();
 const Cors = require('cors');
-const urlParse = require('express');
 const mainRouter = require('./router/Router');
 const PORT = process.env.PORT || 4000;
+const bodyParser = require('body-parser');
+// const urlParser = express.json();
 
 dbConnection();
 
 app.use(Cors());
-app.use(urlParse);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(mainRouter);
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Api is hit');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
